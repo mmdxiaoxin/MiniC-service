c82e6a7d4cb55bd3bfd8d92bcdc932b14307020b
小新 <782446723@qq.com>
2023-12-26 10:38:47 +0800

    初始化仓库

---
+++ src/main/java/com/mmdxiaoxin/minicservice/MiniCServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mmdxiaoxin.minicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmdxiaoxin/minicservice/MiniCServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmdxiaoxin.minicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
